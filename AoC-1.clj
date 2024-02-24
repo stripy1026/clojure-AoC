03b45196508f1dce339566936a4b18694fb58e31
stripy1026 <aequatio1026@gmail.com>
2024-02-25 00:25:27 +0900

    first commit

---
+++ AoC-1.clj
@@ -0,0 +1,20 @@
+(ns AoC-1
+  (:require [clojure.string :as str]))
+
+;;1
+(def read-num
+  (->>
+   (str/split (read-line) #" ")
+   (map #(Integer/parseInt %))))
+
+(reduce + read-num)
+
+;;2
+(def num-cycle (cycle read-num))
+(loop [cur 0
+       nums #{}
+       num-cycle-iter num-cycle]
+  (let [sum (+ cur (first num-cycle-iter))]
+  (if (contains? nums sum)
+    sum 
+    (recur sum (conj nums sum) (next num-cycle-iter)))))
